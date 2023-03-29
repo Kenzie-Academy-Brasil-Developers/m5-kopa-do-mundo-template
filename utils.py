6bef2968a8bc003b1967e19d488d9686f94c875f
Lucas Rodrigues <lcs.rs23@gmail.com>
2023-03-29 09:58:58 -0300

    feat: tarefa 0 finalizada

--- utils.py
+++ utils.py
@@ -1,19 +1,23 @@
+from datetime import datetime as dt
 from exceptions import NegativeTitlesError, InvalidYearCupError, ImpossibleTitlesError
 
 
 def data_processing(teams: dict):
     cup_years = []
     for year in list(range(2023)):
-        if year > 1299 and year%4 == 0:
+        if year > 1929 and (year+2) % 4 == 0:
             cup_years.append(year)
+
     if teams['titles'] < 0:
         raise NegativeTitlesError("titles cannot be negative")
 
-    if teams['first_cup'] in cup_years:
+    if not dt.strptime(teams['first_cup'], '%Y-%m-%d').year in cup_years:
         raise InvalidYearCupError("there was no world cup this year")
-
-    if teams.titles > ((teams['first_cup'] - 2023) / (-4)):
+    
+    if teams['titles'] > ((dt.strptime(teams['first_cup'], '%Y-%m-%d').year - 2023) / (-4)):
         raise ImpossibleTitlesError("impossible to have more titles than disputed cups")
+    
+    return (teams)
 
 
 data = {
@@ -21,7 +25,7 @@ data = {
     "titles": 3,
     "top_scorer": "Zidane",
     "fifa_code": "FRA",
-    "first_cup": "1911-10-18"
+    "first_cup": "1934-10-18"
 }
 
 print(data_processing(data))
