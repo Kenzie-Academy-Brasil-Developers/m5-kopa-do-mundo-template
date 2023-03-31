418e60fcd0022b7ff3301303cf83b084a2d1508c
Petrarca <pedropetrarcar@gmail.com>
2023-03-31 17:10:08 -0300

    feat: tarefa 3 concluida.

--- teams/urls.py
+++ teams/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TeamsView
+from .views import TeamsView, TeamDetailsView
 
 
 urlpatterns = [
-    path("teams/", TeamsView.as_view())
+    path("teams/", TeamsView.as_view()),
+    path("teams/<int:team_id>/", TeamDetailsView.as_view())
 ]
--- teams/views.py
+++ teams/views.py
@@ -13,20 +13,57 @@ class TeamsView(APIView):
         for team in teams:
             team_dict = model_to_dict(team)
             team_list.append(team_dict)
-        return Response(team_list)
+        return Response(team_list, status.HTTP_200_OK)
 
     def post(self, request: Request) -> Response:
         try:
-            data_processing(request.data) 
+            data_processing(request.data)
             team = Team.objects.create(**request.data)
             print(model_to_dict(team))
             team_dict = model_to_dict(team)
             return Response(team_dict, status.HTTP_201_CREATED)
         except NegativeTitlesError:
-            return Response({"error": "titles cannot be negative"}, status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "titles cannot be negative"}, status.HTTP_400_BAD_REQUEST
+            )
         except InvalidYearCupError:
-            return Response({"error": "there was no world cup this year"}, status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "there was no world cup this year"},
+                status.HTTP_400_BAD_REQUEST,
+            )
         except ImpossibleTitlesError:
-            return Response({"error": "impossible to have more titles than disputed cups"}, status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "impossible to have more titles than disputed cups"},
+                status.HTTP_400_BAD_REQUEST,
+            )
         except IntegrityError:
             return Response({"error": "ABREVIACO DE TIME"}, status.HTTP_400_BAD_REQUEST)
+
+
+class TeamDetailsView(APIView):
+    def get(self, request: Request, team_id: int) -> Response:
+        try:
+            team = Team.objects.get(id=team_id)
+            team_dict = model_to_dict(team)
+            return Response(team_dict, status.HTTP_200_OK)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+
+    def patch(self, request: Request, team_id: int) -> Response:
+        try:
+            team = Team.objects.get(id=team_id)
+            for key, value in request.data.items():
+                setattr(team, key, value)
+            team.save()
+            team_dict = model_to_dict(team)
+            return Response(team_dict, status.HTTP_200_OK)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request: Request, team_id: int) -> Response:
+        try:
+            team = Team.objects.get(id=team_id)
+            team.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
