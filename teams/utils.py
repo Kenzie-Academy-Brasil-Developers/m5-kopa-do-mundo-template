b66d35d6fb2c34f89b712727066e4fb06641ba53
Gabriela <gabrielagarciafontoura@gmail.com>
2023-04-02 16:50:58 -0300

    feat: task 1 done

--- teams/utils.py
+++ teams/utils.py
@@ -2,9 +2,13 @@ from exceptions import NegativeTitlesError, InvalidYearCupError, ImpossibleTitle
 
 
 def data_processing(data):
+
     titles = data["titles"]
     first_cup = int(data["first_cup"][:4])
     current_year = 2023
+
+    if type(data) is not dict:
+        raise TypeError("data must be a dict")
 
     if titles < 0:
         raise NegativeTitlesError("titles cannot be negative")
@@ -12,9 +16,8 @@ def data_processing(data):
     if first_cup < 1930 or first_cup > current_year or (first_cup - 1930) % 4 != 0:
         raise InvalidYearCupError("there was no world cup this year")
 
-    max_titles = (current_year - first_cup) // 4 
+    max_titles = (current_year - first_cup) // 4
     if titles > max_titles:
         raise ImpossibleTitlesError("impossible to have more titles than disputed cups")
 
     return data
-
