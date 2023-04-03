656e98ddb2a774ba4c8ab9f07b84f5ebd3e9adfb
Gabriela <gabrielagarciafontoura@gmail.com>
2023-04-03 19:24:47 -0300

    feat task 2 done

--- teams/utils.py
+++ teams/utils.py
@@ -1,4 +1,8 @@
-from exceptions import NegativeTitlesError, InvalidYearCupError, ImpossibleTitlesError
+from teams.exceptions import (
+    ImpossibleTitlesError,
+    InvalidYearCupError,
+    NegativeTitlesError,
+)
 
 
 def data_processing(data):
@@ -11,15 +15,13 @@ def data_processing(data):
         raise TypeError({"error": "data must be a dict"})
 
     if titles < 0:
-        raise NegativeTitlesError({"error": "titles cannot be negative"})
+        raise NegativeTitlesError("titles cannot be negative")
 
     if first_cup < 1930 or first_cup > current_year or (first_cup - 1930) % 4 != 0:
-        raise InvalidYearCupError({"error": "there was no world cup this year"})
+        raise InvalidYearCupError("there was no world cup this year")
 
     max_titles = (current_year - first_cup) // 4
     if titles > max_titles:
-        raise ImpossibleTitlesError(
-            {"error": "impossible to have more titles than disputed cups"}
-        )
+        raise ImpossibleTitlesError("impossible to have more titles than disputed cups")
 
     return data
--- teams/views.py
+++ teams/views.py
@@ -5,6 +5,7 @@ from teams.exceptions import (
     InvalidYearCupError,
     NegativeTitlesError,
 )
+
 from teams.models import Team
 from .utils import data_processing
 
@@ -18,24 +19,24 @@ class TeamView(APIView):
     def post(self, request: Request) -> Response:
         try:
             data = request.data
-            team = Team.objects.create(**data)
+            validate = data_processing(data)
+            team = Team.objects.create(**validate)
             team_dict = model_to_dict(team)
-            data_processing(team_dict)
 
-            return Response(team_dict, status.HTTP_201_CREATED)
+        except NegativeTitlesError as err:
+            return Response(
+                {"error": err.message},
+                status.HTTP_400_BAD_REQUEST,
+            )
+        except InvalidYearCupError as err:
+            return Response(
+                {"error": err.message},
+                status.HTTP_400_BAD_REQUEST,
+            )
+        except ImpossibleTitlesError as err:
+            return Response(
+                {"error": err.message},
+                status.HTTP_400_BAD_REQUEST,
+            )
 
-        except NegativeTitlesError:
-            return Response(
-                {"error": "titles cannot be negative"},
-                status.HTTP_400_BAD_REQUEST,
-            )
-        except InvalidYearCupError:
-            return Response(
-                {"error": "there was no world cup this year"},
-                status.HTTP_400_BAD_REQUEST,
-            )
-        except ImpossibleTitlesError:
-            return Response(
-                {"error": "impossible to have more titles than disputed cups"},
-                status.HTTP_400_BAD_REQUEST,
-            )
+        return Response(team_dict, status.HTTP_201_CREATED)
