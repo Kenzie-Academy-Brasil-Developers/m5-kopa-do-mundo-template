0601149699bde5df8b4dc47c836c2ef48edcd4b2
Cleyviton <cleyvitonkeke@gmail.com>
2023-06-01 11:46:11 -0300

    feat: Tarefa 4 | View Params finalizada

--- teams/views.py
+++ teams/views.py
@@ -22,3 +22,40 @@ class TeamView(APIView):
         teams_dict = [model_to_dict(team) for team in teams]
 
         return Response(teams_dict)
+
+
+class TeamDetailView(APIView):
+    def get(self, request: Request, team_id: int) -> Response:
+        try:
+            team = Team.objects.get(id=team_id)
+        except:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+
+        team_dict = model_to_dict(team)
+
+        return Response(team_dict)
+
+    def patch(self, request: Request, team_id: int) -> Response:
+        try:
+            team = Team.objects.get(id=team_id)
+        except:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+
+        for key, value in request.data.items():
+            setattr(team, key, value)
+
+        team.save()
+
+        team_dict = model_to_dict(team)
+
+        return Response(team_dict)
+
+    def delete(self, request: Request, team_id: int) -> Response:
+        try:
+            team = Team.objects.get(id=team_id)
+        except:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+
+        team.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
