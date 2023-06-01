fc3d25dca9b58264a2ea8c67ffd9ea17b8211bce
Cleyviton <cleyvitonkeke@gmail.com>
2023-06-01 11:24:08 -0300

    feat: Tarefa 3 | Views finalizada

---
+++ teams/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import TeamView
+
+
+urlpatterns = [
+    path("teams/", TeamView.as_view()),
+]
--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from .models import Team
+from django.forms.models import model_to_dict
+from utils import data_processing
 
-# Create your views here.
+
+class TeamView(APIView):
+    def post(self, request: Request) -> Response:
+        try:
+            data_processing(request.data)
+        except Exception as e:
+            message_error = str(e)
+            return Response({"error": message_error}, status.HTTP_400_BAD_REQUEST)
+
+        team = Team.objects.create(**request.data)
+        team_dict = model_to_dict(team)
+
+        return Response(team_dict, status.HTTP_201_CREATED)
+
+    def get(self, request: Request) -> Response:
+        teams = Team.objects.all()
+        teams_dict = [model_to_dict(team) for team in teams]
+
+        return Response(teams_dict)
