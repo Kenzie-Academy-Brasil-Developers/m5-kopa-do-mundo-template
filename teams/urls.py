c0ddfaee0a743cd3bf25d3ddc739dee0e24be426
Yuri Mota <yurirm55@gmail.com>
2023-06-01 18:52:40 -0300

    feat: tarefa 4 finalizada

--- teams/urls.py
+++ teams/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [path("teams/", TeamsView.as_view())]
+urlpatterns = [
+    path("teams/", TeamsView.as_view()),
+    path("teams/<int:team_id>/", TeamsIdView.as_view()),
+]
--- teams/views.py
+++ teams/views.py
@@ -25,3 +25,35 @@ class TeamsView(APIView):
             team_dict = model_to_dict(team)
             teams_list.append(team_dict)
         return Response(teams_list, status.HTTP_200_OK)
+
+
+class TeamsIdView(APIView):
+    def get(self, request: Request, team_id: int) -> Response:
+        try:
+            team = Team.objects.get(id=team_id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+
+        team_dict = model_to_dict(team)
+        return Response(team_dict, status.HTTP_200_OK)
+
+    def delete(self, request: Request, team_id: int) -> Response:
+        try:
+            team = Team.objects.get(id=team_id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+        team.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request: Request, team_id: int) -> Response:
+        try:
+            team = Team.objects.get(id=team_id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+
+        for key, value in request.data.items():
+            setattr(team, key, value)
+
+        team.save()
+        team_dict = model_to_dict(team)
+        return Response(team_dict, status.HTTP_200_OK)
