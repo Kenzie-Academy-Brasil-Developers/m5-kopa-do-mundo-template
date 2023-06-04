90420788e9b395bca015259075f1038fdec2a56d
Gabriel Sales <gabrielsales081@gmail.com>
2023-06-04 16:56:06 -0300

    feat: tarefa 3 finalizada

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from .models import Team
+from django.forms.models import model_to_dict
+from utils import data_processing, ImpossibleTitlesError, InvalidYearCupError, NegativeTitlesError
 
-# Create your views here.
+
+class TeamView(APIView):
+    def get(self, req: Request) -> Response:
+        teams = Team.objects.all()
+        teams_list = []
+
+        for team in teams:
+            teams_dict = model_to_dict(team)
+            teams_list.append(teams_dict)
+
+        return Response(teams_list, status.HTTP_200_OK)
+
+    def post(self, req: Request) -> Response:
+        try:
+            data_processing(req.data)
+        except ImpossibleTitlesError as error:
+            return Response({"error": str(error)}, status.HTTP_400_BAD_REQUEST)
+        except InvalidYearCupError as error:
+            return Response({"error": str(error)}, status.HTTP_400_BAD_REQUEST)
+        except NegativeTitlesError as error:
+            return Response({"error": str(error)}, status.HTTP_400_BAD_REQUEST)
+
+        team = Team.objects.create(**req.data)
+        team_dict = model_to_dict(team)
+
+        return Response(team_dict, status.HTTP_201_CREATED)
