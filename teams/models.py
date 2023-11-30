5cdb1fb575843706d88cbef0f7da5878e9845171
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-29 22:21:32 -0300

    feat: Classe Team

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 
+class Team(models.Model):
+    name = models.CharField(max_length=30, null=False)
+    titles = models.PositiveIntegerField(default=0, null=True)
+    top_scorer = models.CharField(max_length=50, null=False)
+    fifa_code = models.CharField(max_length=3, unique=True, null=False)
+    first_cup = models.DateField(null=True, blank=True)
+
+    def __repr__(self) -> str:
+        return f"<[{self.id}] {self.name} - {self.fifa_code}>"
