60ac380e8e2599dc579794c61ceb3513a104ca1c
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-12-01 16:39:47 -0300

    feat: Rotas

---
+++ teams/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("teams/", views.TeamView.as_view()),
+    path("teams/<int:team_id>/", views.TeamDetailView.as_view()),
+]
