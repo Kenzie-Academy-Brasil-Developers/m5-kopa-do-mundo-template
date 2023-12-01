b96d3090ad629c607c9358f0e08073acdba3ca23
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-12-01 16:45:40 -0300

    feat: Team POST

--- teams/views.py
+++ teams/views.py
@@ -1,1 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, status, Request, Response
+from teams.models import Team
+from django.forms import model_to_dict
+from utils import data_processing
+from exceptions import NegativeTitlesError, ImpossibleTitlesError, InvalidYearCupError
+
+
+class TeamView(APIView):
+    def post(self, request: Request) -> Response:
+        try:
+            received_data = data_processing(request.data)
+            team = Team.objects.create(**received_data)
+            converted_team = model_to_dict(team)
+            return Response(converted_team, status.HTTP_201_CREATED)
+        except (
+            NegativeTitlesError,
+            ImpossibleTitlesError,
+            InvalidYearCupError,
+        ) as err:
+            return Response({"error": err.args[0]}, status.HTTP_400_BAD_REQUEST)
+
