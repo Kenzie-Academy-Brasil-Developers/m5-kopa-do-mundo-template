e170cd5687feeef80b8d07b3b930f1fdd8e608be
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-12-01 16:42:53 -0300

    fix: Correção em tipagem de titles em Team

--- teams/models.py
+++ teams/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=30, null=False)
-    titles = models.PositiveIntegerField(default=0, null=True)
+    titles = models.IntegerField(default=0, null=True)
     top_scorer = models.CharField(max_length=50, null=False)
     fifa_code = models.CharField(max_length=3, unique=True, null=False)
     first_cup = models.DateField(null=True, blank=True)
